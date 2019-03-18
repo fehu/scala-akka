b39f8c8a7d56aaf407af20b9c7b370d3f1825a29
Dmitry Kovalev <kdn.kovalev@gmail.com>
2019-03-18 17:36:10 -0600

    added `TracedActorRef` and `TracingActorRefOps`

---
+++ src/main/scala/io/opentracing/contrib/akka/package.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 Dmitry K
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.opentracing.contrib
+
+import scala.concurrent.Future
+
+import _root_.akka.actor.{ Actor, ActorContext, ActorRef }
+import _root_.akka.pattern.ask
+import _root_.akka.util.Timeout
+import io.opentracing.contrib.akka.TracedMessage.{ MaybeSpan, wrap }
+
+
+package object akka {
+  implicit class TracingActorRefOps(ref: ActorRef) {
+    def *!(msg: TracedMessage)(implicit sender: ActorRef = Actor.noSender): Unit = ref ! msg
+    def *!(msg: Any)(implicit span: MaybeSpan, sender: ActorRef = Actor.noSender): Unit = ref ! wrap(msg)
+
+    def *?(msg: TracedMessage)(implicit timeout: Timeout, sender: ActorRef = Actor.noSender): Future[Any] = ref ? msg
+    def *?(msg: Any)(implicit span: MaybeSpan, timeout: Timeout, sender: ActorRef = Actor.noSender): Future[Any] = ref ? wrap(msg)
+
+    def forwardTr(traced: TracedMessage)(implicit context: ActorContext): Unit = *!(traced)(context.sender())
+    def forwardTr(msg: Any)(implicit span: MaybeSpan, context: ActorContext): Unit = *!(msg)(span, context.sender())
+  }
+}
