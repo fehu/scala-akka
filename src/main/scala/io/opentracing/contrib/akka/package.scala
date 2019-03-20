67b796d0be7330a5ac944f4ca1c6a3c60646c36f
Dmitry Kovalev <kdn.kovalev@gmail.com>
2019-03-20 16:15:27 -0600

    removed `TracingActorRefOps`

--- src/main/scala/io/opentracing/contrib/akka/package.scala
+++ src/main/scala/io/opentracing/contrib/akka/package.scala
@@ -13,23 +13,7 @@
  */
 package io.opentracing.contrib
 
-import scala.concurrent.Future
-
-import _root_.akka.actor.{ Actor, ActorContext, ActorRef }
-import _root_.akka.pattern.ask
-import _root_.akka.util.Timeout
-import io.opentracing.Span
-
 
 package object akka {
-  implicit class TracingActorRefOps(ref: ActorRef) {
-    def *!(msg: TracedMessage)(implicit sender: ActorRef = Actor.noSender): Unit = ref ! msg
-    def *!(msg: Any)(implicit span: Span, sender: ActorRef = Actor.noSender): Unit = ref ! TracedMessage.wrap(msg)
 
-    def *?(msg: TracedMessage)(implicit timeout: Timeout, sender: ActorRef = Actor.noSender): Future[Any] = ref ? msg
-    def *?(msg: Any)(implicit span: Span, timeout: Timeout, sender: ActorRef = Actor.noSender): Future[Any] = ref ? TracedMessage.wrap(msg)
-
-    def forwardTr(traced: TracedMessage)(implicit context: ActorContext): Unit = *!(traced)(context.sender())
-    def forwardTr(msg: Any)(implicit span: Span, context: ActorContext): Unit = *!(msg)(span, context.sender())
-  }
 }
